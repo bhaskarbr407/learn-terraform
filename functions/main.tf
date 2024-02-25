1325f332355e3e58006308094414295253dbf3ac
Durga <bhaskarbr407@gmail.com>
2024-02-25 23:45:19 +0530

    functions terraform

--- functions/main.tf
+++ functions/main.tf
@@ -5,3 +5,16 @@ variable "sample" {
 output "sample" {
   value = upper(var.sample)
 }
+variable "sample1" {
+  default = ["abc", "xyz"]
+  }
+
+variable "sample2"{
+  default ={
+    zbc = 10
+    xyz = 23
+  }
+}
+output "sample1" {
+  value = element(var.sample1, 0)
+}

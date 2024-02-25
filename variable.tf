b4ca2ceec04eb05d2a5020fddead50193c5c9b0a
Durga <bhaskarbr407@gmail.com>
2024-02-25 14:31:35 +0530

    servers terraform

--- variable.tf
+++ variable.tf
@@ -2,5 +2,9 @@ variable "sample_string"{
 default = "hello world"
 }
 output "sample_string"{
-value=var.sample_string
+value= var.sample_string
+}
+
+output "sample_string1" {
+  value = "value of sample_string = ${ var.sample_string}"
 }
